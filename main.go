a7a3a81a902a9df9e66b29ae8102064dc6b43938
Dmitry T <dmitry_dtv@mail.ru>
2022-12-03 20:08:40 +0500

    4func

--- main.go
+++ main.go
@@ -3,14 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	add()
+	var firstnumber, secondnumber int
+	fmt.Println("Введите два числа:\nВведите первое чило:")
+	fmt.Scan(&firstnumber)
+	fmt.Println("Введите второе число:")
+	fmt.Scan(&secondnumber)
+
+	resultOfadding := adding(firstnumber, secondnumber)
+	resultOfmulti := multi(firstnumber, secondnumber)
+	resultOfdiv := div(firstnumber, secondnumber)
+	resultOfminus := minus(firstnumber, secondnumber)
+	fmt.Println("Произведение числа a и b:", resultOfmulti)
+	fmt.Println("Сложение  a и b:", resultOfadding)
+	fmt.Println("Деление чисел a и b:", resultOfdiv)
+	fmt.Println("ВЫчитание чисел a и b:", resultOfminus)
+}
+func adding(a int, b int) (z int) {
+	z = a + b
+	return z
 }
 
-func add() {
-	var a, b int
-	fmt.Println("Введите два числа:\nВведите первое чило:")
-	fmt.Scan(&a)
-	fmt.Println("Введите второе число:")
-	fmt.Scan(&b)
-	fmt.Println("Произведение числа a и b:", a*b)
+func multi(a int, b int) (z int) {
+	z = a * b
+	return z
+}
+func div(a int, b int) (z int) {
+	z = a / b
+	return z
+}
+func minus(a int, b int) (z int) {
+	z = a - b
+	return z
 }
